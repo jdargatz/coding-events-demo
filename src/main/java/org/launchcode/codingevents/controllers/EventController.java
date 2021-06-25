ddf3d0aa1a807af8ab144b9068d77e9799750880
jdargatz <julie.dargatz@gmail.com>
2021-06-24 20:09:36 -0500

    Finished exercise 10

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -21,7 +22,13 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Whiskey Tasting", "Taste the inside of an aged barrel.");
+        events.put("Potato Gun Competition", "Fire those potatoes as far as you can!");
+        events.put("Hot Dog Eating Competition", "How many dogs can you shove in your mouth?");
         model.addAttribute("events", events);
         return "events/index";
     }
